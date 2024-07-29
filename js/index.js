667e5ce9f07cbcfc599044505e549e46d479b34d
lllakon <CoffeWhite58@gmail.com>
2024-07-29 20:56:07 +0200

    Added changing text and arrows when clicking on the more button
    -

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
 let brandCardsContainer = document.querySelector(".brandCards")
 let showMoreBtn = document.querySelector(".showMore")
+let showMoreText = document.querySelector(".showMore__text")
+let showArrow = document.querySelector(".showMore__arrow")
 
 showMoreBtn.addEventListener("click", () => {
 	if (brandCardsContainer.style.overflow === "visible") {
+		showMoreText.textContent = "Показать все"
+		showArrow.classList.remove("showMore__arrow--visible")
 		brandCardsContainer.style.height = "155px"
 		brandCardsContainer.style.overflow = "hidden"
 	} else {
+		showMoreText.textContent = "Скрыть"
+		showArrow.classList.add("showMore__arrow--visible")
 		brandCardsContainer.style.height = "auto"
 		brandCardsContainer.style.overflow = "visible"
 	}
